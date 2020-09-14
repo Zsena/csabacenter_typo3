2d07e028a563546c064460c3ee4a8ec04d799657
András Ottó <andras.otto@digital-zombies.com>
2020-09-14 11:12:31 +0200

    Set ImageMagick for production

--- www/html/public/typo3conf/AdditionalConfiguration.Production.php
+++ www/html/public/typo3conf/AdditionalConfiguration.Production.php
@@ -7,3 +7,17 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host']     = '$DB_H
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = '$DB_PW';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user']     = '$DB_USER';
 $GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword']                = '$INSTALL_TOOL_PW';
+
+
+$GLOBALS['TYPO3_CONF_VARS']['GFX'] = [
+    'gdlib_png' => false,
+    'processor' => 'ImageMagick',
+    'processor_allowTemporaryMasksAsPng' => false,
+    'processor_colorspace' => 'sRGB',
+    'processor_effects' => true,
+    'processor_enabled' => true,
+    'processor_path' => '/usr/bin/',
+    'processor_path_lzw' => '/usr/bin/',
+    'jpg_quality' => 85,
+    'processor_stripColorProfileCommand' => '+profile \'*\' -interlace plane -strip',
+];
