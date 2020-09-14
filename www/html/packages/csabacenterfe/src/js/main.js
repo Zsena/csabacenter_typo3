d55b1a56f16941a8a81cbdf4c493b3072cc6b4e2
András Ottó <andras.otto@digital-zombies.com>
2020-09-14 10:14:23 +0200

    Reworked the filter js to the new markup

--- www/html/packages/csabacenterfe/src/js/main.js
+++ www/html/packages/csabacenterfe/src/js/main.js
@@ -372,8 +372,8 @@ window.jQuery = window.$ = require('jquery');
 
             // Executes the func after delay time.
             searchEvent  =  setTimeout(function(){
-                $('.services_blog').parent().fadeOut(500);
-                $('.services_blog h4:Contains("' + searchWord + '")').parent().parent().fadeIn(500);
+                $('.post-entry-1').parent().fadeOut(500);
+                $('.post-entry-1 h2:Contains("' + searchWord + '")').parent().parent().parent().fadeIn(500);
             }, delay)
         }
         $('#shopSelect').on('change', function () {
